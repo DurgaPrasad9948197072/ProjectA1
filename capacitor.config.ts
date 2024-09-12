15270d5b65d3927c43ad276c36065927de235fbc
Durgaprasad <kanuri.durgaprasad1997@gmail.com>
2024-09-12 23:34:27 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Mixed',
+  webDir: 'www'
+};
+
+export default config;
